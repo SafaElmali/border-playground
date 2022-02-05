3f615d0586c3a1b901a32e16b09842f001153d90
SafaElmali <tsafaelmali@gmail.com>
2022-02-05 12:48:20 +0300

    Implement vite.js

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    html,body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Inter', sans-serif;
+    }
+`;
+
+export default GlobalStyles;
